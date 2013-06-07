a30ca06fc12f4152fada4b4479c5a7b2c86380ef
John Spackman <john.spackman@zenesis.com>
2013-06-07 13:47:44 +0100

    adding build.sh for contribs

---
+++ build.sh
@@ -0,0 +1,26 @@
+
+function rewriteManifest {
+	node << __EOF__
+	var fs = require('fs');
+	fs.readFile('Manifest.json', function(err, data) {
+		var json = JSON.parse(data);
+		json.info.checksum = "$1";
+		fs.writeFile('Manifest.json', JSON.stringify(json, null, 2));
+	});
+__EOF__
+}
+
+mkdir dist
+rm -f dist/dist.zip
+
+rewriteManifest ""
+zip -r dist/dist.zip * -x dist
+SHA=`shasum dist/dist.zip`
+echo SHA=$SHA
+SHA=${SHA/%  dist\/dist.zip/}
+echo SHA=$SHA
+rewriteManifest "$SHA"
+
+git add dist/dist.zip
+git commit -m 'building distribution'
+git push
